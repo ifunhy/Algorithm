1652abbc2d886cc771208d4a607cde134ef78967
ifunhy <guswl8296@gmail.com>
2025-05-20 08:50:48 +0900

    [Bronze V] Title: A+B, Time: 180 ms, Memory: 17712 KB -BaekjoonHub

--- 백준/Bronze/1000. A＋B/A＋B.java
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -1,9 +1,11 @@
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int A = scanner.nextInt();
-        int B = scanner.nextInt();
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+
         System.out.println(A + B);
     }
 }
