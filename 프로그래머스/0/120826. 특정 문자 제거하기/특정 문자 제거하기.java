301011d53a55ae31cdd381d1e09da02ee82cc1ef
JIKI <guswl8296@gmail.com>
2025-06-12 11:14:50 +0900

    [level 0] Title: 특정 문자 제거하기, Time: 4.52 ms, Memory: 75 MB -BaekjoonHub

--- 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.java
+++ 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.java
@@ -1,9 +1,15 @@
 class Solution {
     public String solution(String my_string, String letter) {
         String answer = "";
+        // for (int i = 0; i < my_string.length(); i++) {
+        //     answer = my_string.replace(letter, "");
+        // }
+        // return answer;
         for (int i = 0; i < my_string.length(); i++) {
-            answer = my_string.replace(letter, "");
+            if (!my_string.substring(i, i + 1).equals(letter)) {
+                answer += my_string.substring(i, i + 1);
+            }
         }
-        return answer;
+        return (answer);
     }
 }
