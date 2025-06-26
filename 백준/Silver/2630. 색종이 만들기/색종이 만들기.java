2d74e61936cf9c4171674c05f8ea114be3decb4d
JIKI <guswl8296@gmail.com>
2025-06-26 17:45:44 +0900

    [Silver II] Title: 색종이 만들기, Time: 144 ms, Memory: 15528 KB -BaekjoonHub

---
+++ 백준/Silver/2630. 색종이 만들기/색종이 만들기.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int white = 0;
+    static int blue = 0;
+    static int[][] paper;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        paper = new int[N][N];
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                paper[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        quarter(0, 0, N);
+        System.out.println(white);
+        System.out.println(blue);
+    }
+
+    public static void quarter(int x, int y, int n) {
+        boolean flag = true;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (paper[x][y] != paper[x+i][y+j]) {
+                    flag = false;
+                    break;
+                }
+                if(!flag) break;
+            }
+        }
+        if (flag == true) {
+            if (paper[x][y] == 0)
+                white++;
+            else
+                blue++;
+        } else {
+            quarter(x, y, n/2);
+            quarter((x + n / 2), y, n/2);
+            quarter(x, (y + n / 2), n/2);
+            quarter((x + n / 2), (y + n / 2), n/2);
+        }
+    }
+}
