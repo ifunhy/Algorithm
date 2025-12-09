9fdd16f712b90ac30dae1e8480899b7ad50cd642
JIKI <guswl8296@gmail.com>
2025-12-09 12:11:14 +0900

    [Bronze II] Title: 소수 찾기, Time: 104 ms, Memory: 14112 KB -BaekjoonHub

---
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int decimal = 0;
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+
+            if (isPrime(arr[i])) {
+                decimal += 1;
+            }
+        }
+        System.out.println(decimal);
+    }
+
+    static boolean isPrime(int n) {
+        if (n == 1) {
+            return false;
+        }
+        for (int i = 2; i * i <= n; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
