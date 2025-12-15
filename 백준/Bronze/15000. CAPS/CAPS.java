78d2bbbb151a542402ade27585bcec467e1b4374
JIKI <guswl8296@gmail.com>
2025-12-15 12:22:41 +0900

    [Bronze V] Title: CAPS, Time: 3672 ms, Memory: 125812 KB -BaekjoonHub

---
+++ 백준/Bronze/15000. CAPS/CAPS.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String lower = br.readLine();
+        String[] arr = new String[lower.length()];
+
+        for (int i = 0; i < lower.length(); i++) {
+            arr[i] = String.valueOf((char)(lower.charAt(i) - 32));
+            System.out.print(arr[i]);
+        }
+    }
+}
