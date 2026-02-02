6cdf1522aa899efd1d5ed4d347ce310dc1a963c1
JIKI <guswl8296@gmail.com>
2026-02-02 10:56:55 +0900

    [Bronze V] Title: 특별한 학교 이름, Time: 100 ms, Memory: 14000 KB -BaekjoonHub

---
+++ 백준/Bronze/27889. 특별한 학교 이름/특별한 학교 이름.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Map<String, String> map = new HashMap<>();
+
+        map.put("NLCS", "North London Collegiate School");
+        map.put("BHA", "Branksome Hall Asia");
+        map.put("KIS", "Korea International School");
+        map.put("SJA", "St. Johnsbury Academy");
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println(map.get(br.readLine()));
+    }
+}
