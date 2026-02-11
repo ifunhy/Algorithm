70c423a10616a13ece43307f1296eeb2044f5e69
JIKI <guswl8296@gmail.com>
2026-02-11 12:22:16 +0900

    [Bronze V] Title: Christmas Tree Adapter, Time: 92 ms, Memory: 14200 KB -BaekjoonHub

---
+++ 백준/Bronze/32314. Christmas Tree Adapter/Christmas Tree Adapter.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int ampere = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int watt = Integer.parseInt(st.nextToken());
+        int volt = Integer.parseInt(st.nextToken());
+
+        if (ampere * volt <= watt) {
+            System.out.println("1");
+        } else {
+            System.out.println("0");
+        }
+    }
+}
